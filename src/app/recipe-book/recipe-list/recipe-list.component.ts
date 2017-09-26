0a711cca65a6f1e1e3eafe2e099c65eee41c2209
Andreas Landerer <andreas.landerericloud.com>
2017-09-26 15:01:48 +0200

    Lecture 47: Building first version of recipe list

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe("My Recipe 1", "My Description 1", "https://goo.gl/2YPdq8"),
+    new Recipe("My Recipe 2", "My Description 2", "https://goo.gl/cfNznn")
+  ];
 
   constructor() { }
 
