646981c16f8aa6f033a25a0c7b3fce4243a7161a
Andreas Landerer <andreas.landerericloud.com>
2017-09-27 19:32:54 +0200

    Lecture 53: Building rest of the components

--- src/app/shopping-list/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list/shopping-list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatos', 9),
+    new Ingredient('Bananas', 2)
+  ];
 
   constructor() { }
 
